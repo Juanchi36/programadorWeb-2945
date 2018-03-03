b7de2ebfe96a09c7cd02902846548816e3747dd5
Juanchi36 <juanmartingon@hotmail.com>
2018-03-03 18:14:31 -0300

    Subo desafio clase 2

---
+++ Clase 02/Alumno/Desafío/index.js
@@ -0,0 +1,9 @@
+var daysOfTheWeek = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+
+for(var i = 0; i < daysOfTheWeek.length; i++){
+    if(daysOfTheWeek[i] === 'Sábado' || daysOfTheWeek[i] === 'Domingo'){
+        console.log(daysOfTheWeek[i], ' es fin de semana!!!')
+    }else{
+        console.log(daysOfTheWeek[i], ' no es fin de semana')
+    }
+}
