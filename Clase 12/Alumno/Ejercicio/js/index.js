a9f23d4280a3416665d5538a0872761b9db75a1b
Juanchi36 <juanmartingon@hotmail.com>
2018-04-07 16:00:47 -0300

    ..

--- Clase 12/Alumno/Ejercicio/js/index.js
+++ Clase 12/Alumno/Ejercicio/js/index.js
@@ -1,10 +1,3 @@
-// # Crear un ta te ti
-
-// - Se deberá utilizar el HMTL, CSS e imágenes incluidas en esta carpeta (no es necesario la imagen).
-// - Cada vez que el usuario hace click sobre un cuadrado se tiene que agregar la clase `circle` o la clase `cross`, alternando entre una u otra.
-
-// [Solución](https://www.useloom.com/share/97d9a0dd3ba44cfda158606c0a4f793b)
-
 var squaresNodes = $('.square')
 var flag = true
 var i = 0
@@ -24,6 +17,5 @@ squaresNodes.click(function (event) {
     i = 0
     squaresNodes.removeClass()
     squaresNodes.addClass('square')
-    console.log('fin')
   }
 })
